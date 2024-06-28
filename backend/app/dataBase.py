96356f9616343458a40b9df843063f4149ccf653
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2024-06-28 23:01:20 +0300

    editoring

--- backend/app/dataBase.py
+++ backend/app/dataBase.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@db:5432/job_parser"
+DATABASE_URL = "postgresql://user:password@db:5432/job_parser"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL) # Движок БД  для установления соединений с БД
+engine = create_engine(DATABASE_URL) # Движок БД  для установления соединений с БД
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # Изменения в сессии не будут автоматически фиксироваться и сбрасываться
 
 Base = declarative_base()
