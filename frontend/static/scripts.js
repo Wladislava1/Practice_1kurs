0760bf3ece0e1eab6aa1dcf6fee40259a132395f
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2024-07-05 22:41:49 +0300

    несколько параметров парсинга на фронте

--- frontend/static/scripts.js
+++ frontend/static/scripts.js
@@ -1,20 +1,40 @@
 document.getElementById('searchForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    const query = document.getElementById('searchQuery').value;
+
+    const title = document.getElementById('searchQuery').value;
+    const schedule = document.getElementById('searchSchedule').value;
+    const salary = parseInt(document.getElementById('searchSalary').value);
+    const experience = document.getElementById('searchExperience').value;
+
+    const jobRequest = {
+        title: title,
+        schedule: schedule,
+        salary: isNaN(salary) ? null : salary, 
+        experience: experience
+    };
 
     // Отправка запроса на сервер для парсинга вакансий и добавления их в БД
-    fetch('http://localhost:8000/parse_job/', { // Указываем порт 8000
+    fetch('http://localhost:8000/parse_job/', {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({ title: query, schedule: "", salary: 0, experience: "" }),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(jobRequest)
     })
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            return response.json().then(errorData => {
+                throw new Error(JSON.stringify(errorData));
+            });
+        }
+        return response.json();
+    })
     .then(data => {
         console.log('Success:', data);
         // Перенаправление на страницу со списком вакансий
         window.location.href = '/jobs.html';
     })
-    .catch((error) => {
+    .catch(error => {
         console.error('Error:', error);
     });
 });
